6941e45cf4dafd0a770eaa57ff2a6230caa3b95b
David <hsam46@gmail.com>
2017-01-05 10:18:48 +0800

    Add Android OTT

---
+++ js/common.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+
+  $('.navbar').append('<a class="hide-phone" id="logo" href="#"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Nyanpass<span>Tsudere</span></a><ul class="menu"><li><a href="index.html">首頁</a></li><li><a href="#">專案</a>	<ul><li><a href="android_ott.html">視界真奇妙</a></li>		<li><a href="watchforyou.html">Wath For You</a></li></ul></li><li><a href="#">關於</a></li></ul>');
+});
